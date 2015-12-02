3ef108cc6d3ee22d4be2b22bb24abd74425d1721
comraq <yinadam@hotmail.com>
2015-12-01 21:32:31 -0800

    Added converted binary images as well as txt file of sample binary image with rgb values

--- convert.cpp
+++ convert.cpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -19,9 +20,30 @@ int main() {
   }
 
   cv::Mat bwImage;
+  cv::Mat binImage;
   cvtColor(sourceImage, bwImage, CV_BGR2GRAY);
+  cv::threshold(bwImage, binImage, 254, 255, CV_THRESH_BINARY);
 
+  //Saving the converted GrayScale and Binary images
   cv::imwrite(MAZEPATH + "Gray" + mazeName, bwImage);
+  cv::imwrite(MAZEPATH + "Binary" + mazeName, binImage);
+
+  fstream binImageText;
+  string outPath = MAZEPATH + "Binary" + mazeName.substr(0, mazeName.length() - 4) + "Text.txt";
+  binImageText.open(outPath.c_str(), fstream::out);
+
+  unsigned char *input = (unsigned char*)(binImage.data);
+
+  int i, j;
+  for (int i = 0; i < binImage.cols; i++) {
+    for (int j = 0; j < binImage.rows; j++) {
+      binImageText << (int)input[binImage.cols * j + i] << " ";
+      binImageText << (int)input[binImage.cols * j + i + 1] << " ";
+      binImageText << (int)input[binImage.cols * j + i + 2] << "\n";
+    }
+  }
+
+  binImageText.close();
 
   return 0;
 }
